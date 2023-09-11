cd7563d0d1e118f31f584ca7f956925841e2261c
Sergei Borisov <bmwth2311@gmail.com>
2023-09-11 23:38:49 +0300

    Initial commit

---
+++ src/main/java/ru/hogwarts/school/controller/StudentController.java
@@ -0,0 +1,19 @@
+package ru.hogwarts.school.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.hogwarts.school.service.StudentService;
+
+@RestController
+@RequestMapping("/student")
+public class StudentController {
+
+    private final StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+
+}
